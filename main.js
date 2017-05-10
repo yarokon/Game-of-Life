072d7750a34472f595d85b778f476bb178b99162
yarokon <yarokon1@gmail.com>
2017-05-10 21:15:43 +0300

    breathe life

--- main.js
+++ main.js
@@ -2,16 +2,21 @@
   const canvas = document.getElementById('game'),
         ctx = canvas.getContext('2d');
 
+  const screen = {
+    w: window.innerWidth,
+    h: window.innerHeight
+  }
+
   const gridSettings = {
-    percent: 25,
-    step: 5,
+    percent: 15,
+    step: 10,
     lineWidth: 1,
     lineColor: '#9e9e9e',
     get xN() {
-      return (window.innerWidth - this.borderWidth * 2 + this.lineWidth) / this.step ^ 0
+      return (screen.w - this.borderWidth * 2 + this.lineWidth) / this.step ^ 0
     },
     get yN() {
-      return (window.innerHeight - this.borderWidth * 2 + this.lineWidth) / this.step ^ 0
+      return (screen.h - this.borderWidth * 2 + this.lineWidth) / this.step ^ 0
     },
     get pixel() {
       return this.step - this.lineWidth;
@@ -26,8 +31,6 @@
       return parseInt( getComputedStyle(canvas).borderWidth );
     }
   };
-
-  console.log(gridSettings.xN, gridSettings.yN);
 
   class Point {
     constructor(x, y) {
@@ -45,62 +48,54 @@
   }
 
   class Cell {
-    constructor() {
-      this.resurrect();
+    constructor(id) {
+      const xy = Point.toXY(id);
+      this.id = id;
+      this.x = xy.x;
+      this.y = xy.y;
+
+      state[xy.y][xy.x] = this;
+      this._alive = false;
     }
 
     isAlive() {
       return this._alive;
     }
 
-    resurrect() {
+    drawCell() {
+      const { pixel, step } = gridSettings;
+      ctx.fillRect(this.x * step, this.y * step, pixel, pixel);
+
       this._alive = true;
     }
 
-    kill() {
+    eraseCell() {
+      const { pixel, step } = gridSettings;
+      ctx.clearRect(this.x * step, this.y * step, pixel, pixel);
+
       this._alive = false;
     }
-
-    drawCell(x, y, options) {
-      const { pixel, step } = options;
-      ctx.fillRect(x * step, y * step, pixel, pixel);
-    }
-
-    eraseCell(x, y, options) {
-      const { pixel, step } = options;
-
-      this.kill();
-      ctx.clearRect(x * step, y * step, pixel, pixel);
-    }
   }
 
-  const state = createInitialState(gridSettings);
+  /*** Initialize the Game ***/
 
-  setCanvasSize(gridSettings);
-  drawLines(gridSettings, ctx);
-  seedState(gridSettings, state);
-  display(gridSettings);
+  setCanvasSize();
+  drawLines();
 
-  function createInitialState(options) {
-    const { xN, yN } = options,
-          state = [];
+  const state = createState(null);
 
-    for (let i = 0; i < yN; i++) {
-      state[i] = new Array(xN).fill(null);
-    }
+  fillState();
+  breatheLife();
 
-    return state;
-  }
-
-  function setCanvasSize(options) {
-    const { boardWidth, boardHeight } = options;
+  function setCanvasSize() {
+    const { boardWidth, boardHeight } = gridSettings;
 
     canvas.width = boardWidth;
     canvas.height = boardHeight;
   }
 
-  function drawLines(options, ctx) {
-    const { boardWidth: w, boardHeight: h, step, lineWidth, lineColor } = options;
+  function drawLines() {
+    const { boardWidth: w, boardHeight: h, step, lineWidth, lineColor } = gridSettings;
 
     ctx.lineWidth = lineWidth;
     ctx.strokeStyle = lineColor;
@@ -118,9 +113,40 @@
     ctx.stroke();
   }
 
-  // generates a random unique position cells on the board
-  function generateRandomArr(options) {
-    const { xN, yN, percent } = options,
+
+  function createState(stuffing) {
+    const { xN, yN } = gridSettings,
+          state = [];
+
+    for (let i = 0; i < yN; i++) {
+      state[i] = new Array(xN).fill(stuffing);
+    }
+
+    return state;
+  }
+
+  function fillState() {
+      const { xN, yN } = gridSettings,
+      N = xN * yN;
+
+      for (let i = 0; i < N; i++) {
+        new Cell(i);
+      }
+  }
+
+
+  function breatheLife() {
+    const randArr = generateRandomArr(gridSettings);
+
+    randArr.forEach( id => {
+      const { x, y } = Point.toXY(id);
+      state[y][x].drawCell();
+    });
+  }
+
+    // generates a random unique position cells on the board
+  function generateRandomArr() {
+    const { xN, yN, percent } = gridSettings,
           randomArr = [],
           N = xN * yN;
 
@@ -135,26 +161,89 @@
     return randomArr.slice(-N * percent / 100);
   }
 
-  function seedState(options, state) {
-    const randArr = generateRandomArr(options)
+  /*** Game cycle ***/
 
-    randArr.forEach(id => {
-      const point = Point.toXY(id);
-      state[point.y][point.x] = new Cell();
-    });
+  function tick() {
+    const neighbours = countAllNeighbours();
+    updateState(neighbours);
   }
 
-  function display(options) {
-    const { xN, yN } = options;
+  tick.click = true;
+
+  document.body.onclick = () => {
+    if (tick.click) {
+      setInterval(tick, 100);
+      tick.click = false;
+    }
+  }
+
+  function countAllNeighbours() {
+    const {xN, yN} = gridSettings,
+          neighbours = createState(0);
 
     for (let y = 0; y < yN; y++) {
       for (let x = 0; x < xN; x++) {
+        if (state[y][x]) {
+          neighbours[y][x] = countCellNeighbours(new Point(x, y), state);
+        }
+      }
+    }
+
+    return neighbours;
+  }
+
+  function countCellNeighbours(point, state) {
+    let { x, y } = point,
+        count = 0;
+
+    // left top cell
+    x--; y--;
+
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3;  j++) {
+
+        if (i === 1 && j === 1) {
+          x++;
+          continue;
+        }
+
+        if (!state[y]) {
+          x += 3;
+          break;;
+        }
+
+        if (state[y][x]) {
+          count += state[y][x].isAlive();
+        }
+
+        x++;
+      }
+
+      y += 1;
+      x -= 3;
+    }
+
+    return count;
+  }
+
+  // main logic
+  function updateState(neighbours) {
+    const { xN, yN } = gridSettings;
+
+    for (let y = 0; y < yN; y++) {
+      for (let x = 0; x < xN; x++) {
+        const cellNeighbours = neighbours[y][x];
         const cell = state[y][x];
 
-        if (cell && cell.isAlive()) {
-          cell.drawCell(x, y, options);
+        if (cell.isAlive() && cellNeighbours < 2 || cellNeighbours > 3) {
+          cell.eraseCell();
+        }
+
+        if (!cell.isAlive() && cellNeighbours === 3) {
+          cell.drawCell();
         }
       }
     }
   }
+
 })();
